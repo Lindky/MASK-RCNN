7e85472d3380ec0a803e064f45c66c8578e6b690
Lin Yang <linyang@Lins-MacBook-Pro-2.local>
2023-03-09 18:33:55 -0500

    upload the data pre-processing code

---
+++ Mask-RCNN.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+#########################################
+import torch
+import numpy as np
+from torch.utils.data import Dataset, DataLoader, random_split
+import os
+from albumentations import (HorizontalFlip, ShiftScaleRotate, Normalize, Resize, Compose, GaussNoise)
+from albumentations.pytorch import ToTensorV2
+from skimage import io, transform
+import matplotlib.pyplot as plt
+
+##################################
+
+#load functions
+execfile("./functions/data_preprocess.py")
+
+# Get cpu or gpu device for training.
+device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
+print(f"Using {device} device")
+
+#loading the data
+data_dir = '/Users/linyang/Downloads/stage1_train'
+data = Nuclie_data(data_dir)
+
+#data size
+print(data.__len__())
+
+#splitting to trainset and validation set and loading the data with batch size of 10
+trainset, validset = random_split(data, [580, 90])
+train_loader = torch.utils.data.DataLoader(dataset=trainset, batch_size=10, shuffle=True)
+valid_loader = torch.utils.data.DataLoader(dataset=validset, batch_size=10)
+
+#view images and their corresponding combined marks
+plot_img(5)
+
